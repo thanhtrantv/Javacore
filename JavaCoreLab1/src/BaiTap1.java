9ba92c04affd4bfcaf58bd2d5d174c1e6f36632e
thanhtran <helpme.tvt@gmail.com>
2017-12-16 11:03:33 +0700

    init lab1

---
+++ JavaCoreLab1/src/BaiTap1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BaiTap1 {
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+
+	    //For string
+
+	    String text= scan.nextLine();
+
+	    System.out.println(text);
+	}
+}
